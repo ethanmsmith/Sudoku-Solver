8cc52057a7f18969cbc65e5e73e2f1ff232c7f9d
Ethan Smith <element.above@gmail.com>
2014-11-17 11:16:04 -0500

    semi-working parse
    parse mostly done. peers working correctly

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -4,36 +4,95 @@ object Solution extends SudokuLike {
   type T = Board
 
   def parse(str: String): Board = {
-    throw new UnsupportedOperationException("not implemented")
+    var board: Map[(Int, Int), Set[Int]] = Map()
+    var peersLst = peers(0, 0)
+
+    def parseRecurse(str: String, row: Int, col: Int): Unit = {
+      var lst = Set(1,2,3,4,5,6,7,8,9)
+      var kv: ((Int, Int), Set[Int]) = ((row,col), lst)
+      
+      if(str.head != '.') {
+        kv = ((row, col), Set(str.head.asDigit))
+      }
+      else if(str.head == '.') {
+        kv = ((row, col), Set(1,2,3,4,5,6,7,8,9))
+      }
+
+      board = board + kv
+      if(col == 8 && row != 8) {
+        parseRecurse(str.substring(1), row+1, 0)
+      }
+      else if(col != 8 && row !=8) {
+        parseRecurse(str.substring(1), row, col+1)
+      }
+    }
+
+    parseRecurse(str, 0, 0)
+
+
+    // val someOfOne = """Some\(/d\)""".r
+
+    // for((k,v) <- board) {
+      
+    //   var lstTmp: List[Int] = List()
+      
+    //   if(v == List(1,2,3,4,5,6,7,8,9)) {
+    //     k match {
+    //       case (a,b) => peersLst = peers(a,b) 
+    //     }
+    //     for(peer <- peersLst) {
+    //       board.get(k).toString match {
+    //         case someOfOne(_*) => lstTmp = v diff board.get(k).get
+    //         case _ => Unit
+    //       } 
+    //     }
+
+    //   board = board + ((k, lstTmp))
+    //   }
+    
+    // }
+    new Board(board)
   }
 
   // You can use a Set instead of a List (or, any Iterable)
-  def peers(row: Int, col: Int): List[(Int, Int)] = {
-    var tmp: List[(Int, Int)] = List()
+  def peers(row: Int, col: Int): Set[(Int, Int)] = {
+    var tmp: Set[(Int, Int)] = Set()
     for(i <- 0 to 8) {
       if(i != col) {
-        tmp = tmp :+ (row, i)
+        tmp = tmp ++ Set((row, i))
       }
       if(i != row)
-        tmp = tmp :+ (i, col)
+        tmp = tmp ++ Set((i, col))
     }
-    //GET VALUES IN THE SAME BLOCK
+    tmp ++ box(row, col)
+  }
+
+  def box(row: Int, col: Int): Set[(Int, Int)] = {
+    var tmp: Set[(Int, Int)] = Set()
+    var box: (Int, Int) = (row/3, col/3)
+    for (i <- 0 to 8;j <- 0 to 8) {
+      if((i/3,j/3) == (row/3, col/3) && (i,j) != (row, col)) {
+        tmp = tmp ++ Set((i,j))
+      }
+    }
     tmp
   }
 }
 
 // Top-left corner is (0,0). Bottom-right corner is (8,8).
 // You don't have to have a field called available. Feel free to change it.
-class Board(val available: Map[(Int, Int), List[Int]]) extends BoardLike[Board] {
+class Board(val available: Map[(Int, Int), Set[Int]]) extends BoardLike[Board] {
 
-  def availableValuesAt(row: Int, col: Int): List[Int] = {
+  def availableValuesAt(row: Int, col: Int): Set[Int] = {
     // Assumes that a missing value means all values are available. Feel
     // free to change this.
-    available.getOrElse((row, col), 1.to(9).toList)
+    available.getOrElse((row, col), 1.to(9).toSet)
   }
 
   def valueAt(row: Int, col: Int): Option[Int] = {
-    throw new UnsupportedOperationException("not implemented")
+    available.get((row, col)) match {
+      case x : Set() => 
+    }
   }
 
   def isSolved(): Boolean = {
@@ -60,5 +119,9 @@ class Board(val available: Map[(Int, Int), List[Int]]) extends BoardLike[Board] 
 
   def solve(): Option[Board] = {
     throw new UnsupportedOperationException("not implemented")
+  }
+
+  override def toString = {
+    available.toString
   }
 }
---
+++ src/test/scala/TestSuite.scala
@@ -0,0 +1,24 @@
+import Solution._
+import cmpsci220.hw.sudoku._
+
+class TestSuite extends org.scalatest.FunSuite {
+	test("peers testing"){
+		println("peers 0,0")
+		println(peers(0,0))
+		println("peers 2,2")
+		println(peers(2,2))
+		println("peers 4,4")
+		println(peers(4,4))
+		println("peers 6,6")
+		println(peers(6,6))
+		println("peers 8,8")
+		println(peers(8,8))
+		println()
+	}
+	test("parse") {
+		println(parse("..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."))
+	}
+	// test("parse2") {
+	// 	println(parse("2...8.3...6..7..84.3.5..2.9...1.54.8.........4.27.6...3.1..7.4.72..4..6...4.1...3"))
+	// }
+}
