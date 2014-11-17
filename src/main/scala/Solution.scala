7bb15763d9b976b5bf714df9d688aa9431ca0825
Ethan Smith <element.above@gmail.com>
2014-11-17 12:10:31 -0500

    working parse
    looks like parse is working

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -4,18 +4,17 @@ object Solution extends SudokuLike {
   type T = Board
 
   def parse(str: String): Board = {
-    var board: Map[(Int, Int), Set[Int]] = Map()
-    var peersLst = peers(0, 0)
+    var board: Map[(Int, Int), List[Int]] = Map()
+    
 
     def parseRecurse(str: String, row: Int, col: Int): Unit = {
-      var lst = Set(1,2,3,4,5,6,7,8,9)
-      var kv: ((Int, Int), Set[Int]) = ((row,col), lst)
+      var kv: ((Int, Int), List[Int]) = ((row,col), List())
       
       if(str.head != '.') {
-        kv = ((row, col), Set(str.head.asDigit))
+        kv = ((row, col), List(str.head.asDigit))
       }
       else if(str.head == '.') {
-        kv = ((row, col), Set(1,2,3,4,5,6,7,8,9))
+        kv = ((row, col), List(0))
       }
 
       board = board + kv
@@ -26,53 +25,51 @@ object Solution extends SudokuLike {
         parseRecurse(str.substring(1), row, col+1)
       }
     }
-
     parseRecurse(str, 0, 0)
-
-
-    // val someOfOne = """Some\(/d\)""".r
-
-    // for((k,v) <- board) {
+    for((k,v) <- board) {
+      var peersLst: List[(Int, Int)] = List()
+      var lstTmp: Set[Int] = Set(1,2,3,4,5,6,7,8,9)
       
-    //   var lstTmp: List[Int] = List()
-      
-    //   if(v == List(1,2,3,4,5,6,7,8,9)) {
-    //     k match {
-    //       case (a,b) => peersLst = peers(a,b) 
-    //     }
-    //     for(peer <- peersLst) {
-    //       board.get(k).toString match {
-    //         case someOfOne(_*) => lstTmp = v diff board.get(k).get
-    //         case _ => Unit
-    //       } 
-    //     }
+      if(v == List(0)) {
+        k match {
+          case (a,b) => peersLst = peers(a,b)
+          println("peers of" + k + peersLst)
+        }
+        for(peer <- peersLst) {
+          board.get(peer) match {
+            case None => lstTmp
+            case Some(x) => x match { case y::Nil => lstTmp -= y
+                                      case y::z => lstTmp}
+            case _ => Unit
+          } 
+        }
 
-    //   board = board + ((k, lstTmp))
-    //   }
+      board = board + ((k, lstTmp.toList))
+      }
     
-    // }
+    }
     new Board(board)
   }
 
   // You can use a Set instead of a List (or, any Iterable)
-  def peers(row: Int, col: Int): Set[(Int, Int)] = {
-    var tmp: Set[(Int, Int)] = Set()
+  def peers(row: Int, col: Int): List[(Int, Int)] = {
+    var tmp: List[(Int, Int)] = List()
     for(i <- 0 to 8) {
       if(i != col) {
-        tmp = tmp ++ Set((row, i))
+        tmp = tmp ++ List((row, i))
       }
       if(i != row)
-        tmp = tmp ++ Set((i, col))
+        tmp = tmp ++ List((i, col))
     }
     tmp ++ box(row, col)
   }
 
-  def box(row: Int, col: Int): Set[(Int, Int)] = {
-    var tmp: Set[(Int, Int)] = Set()
+  def box(row: Int, col: Int): List[(Int, Int)] = {
+    var tmp: List[(Int, Int)] = List()
     var box: (Int, Int) = (row/3, col/3)
     for (i <- 0 to 8;j <- 0 to 8) {
       if((i/3,j/3) == (row/3, col/3) && (i,j) != (row, col)) {
-        tmp = tmp ++ Set((i,j))
+        tmp = tmp ++ List((i,j))
       }
     }
     tmp
@@ -81,18 +78,16 @@ object Solution extends SudokuLike {
 
 // Top-left corner is (0,0). Bottom-right corner is (8,8).
 // You don't have to have a field called available. Feel free to change it.
-class Board(val available: Map[(Int, Int), Set[Int]]) extends BoardLike[Board] {
+class Board(val available: Map[(Int, Int), List[Int]]) extends BoardLike[Board] {
 
-  def availableValuesAt(row: Int, col: Int): Set[Int] = {
+  def availableValuesAt(row: Int, col: Int): List[Int] = {
     // Assumes that a missing value means all values are available. Feel
     // free to change this.
-    available.getOrElse((row, col), 1.to(9).toSet)
+    available.getOrElse((row, col), 1.to(9).toList)
   }
 
   def valueAt(row: Int, col: Int): Option[Int] = {
-    available.get((row, col)) match {
-      case x : Set() => 
-    }
+    throw new UnsupportedOperationException("one")
   }
 
   def isSolved(): Boolean = {
